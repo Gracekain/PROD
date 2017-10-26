54d8315f4d39ce6c27e8018eb3bc9abd49092ebb
Heliton <heliton_rudolf@estudante.sc.senai.br>
2017-10-25 21:31:03 -0300

    Conexão com o banco de dados sqlite, fechando issue - closes #4

--- Biblioteca/src/javaconnect.java
+++ Biblioteca/src/javaconnect.java
@@ -1,18 +1,14 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import javax.swing.JOptionPane;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 
 /**
  *
  * @author Heliton
  */
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 public class javaconnect {
     Connection conn;
     
