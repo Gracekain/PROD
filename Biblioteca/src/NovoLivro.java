0aee38aa27f1bc97d511e8b4f8a0399d1c5b6368
Jonas <jonas_g_pereira@estudante.sc.senai.br>
2017-11-07 20:47:37 -0300

    Configuração das classes Estudante e NovoLivro Limpando Campos de Adição - Removendo bug

--- Biblioteca/src/NovoLivro.java
+++ Biblioteca/src/NovoLivro.java
@@ -207,7 +207,7 @@ public class NovoLivro extends javax.swing.JFrame {
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
         }
-        jTextField1.setText("");
+        Random();
         jTextField2.setText("");
         jTextField3.setText("");
         jTextField4.setText("");
